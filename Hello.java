a8bce203c1073fd1cf8b6bb91e6c3c9e6e19f86b
Bekarys <230103246@sdu.edu.kz>
2025-09-17 21:05:11 +0500

    Task 2

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello DevOps. I am Bekarys");
+        System.out.println("Hello DevOps. I am Task 2");
     }
 }
